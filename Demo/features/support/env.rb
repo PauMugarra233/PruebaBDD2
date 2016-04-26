d9a5149e13894c6db7714a94b658cee48909665a
PauMugarra233 <pau.mugarra@233gradosdeti.com>
2016-04-26 13:18:04 +0200

    Update env.rb

--- Demo/features/support/env.rb
+++ Demo/features/support/env.rb
@@ -6,7 +6,7 @@ require 'rspec'
 Capybara.run_server = false
 Capybara.default_driver = :selenium
 Capybara.default_selector = :css
-Selenium::WebDriver::Firefox::Binary.path=/home/ec2-user/firefox
+Selenium::WebDriver::Firefox::Binary.path='/home/ec2-user/firefox'
 
 module Helpers
   def without_resynchronize
